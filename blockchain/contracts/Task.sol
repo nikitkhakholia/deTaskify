6701f5134c095e504242330433ae7fe40328cf58
nikitkhakholia <khakholia.nk@gmail.com>
2022-11-15 19:09:46 +0530

    import solved

--- blockchain/contracts/Task.sol
+++ blockchain/contracts/Task.sol
@@ -1,29 +1,46 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.6;
 
-contract Task {
-    uint256 id;
-    uint256 createdBy;
-    uint256 assignedTo;
-    uint8 priority;
-    bool completed;
-    bool pinned;
-    string name;
-    string description;
-    Task[] subTask;
-    string[] labels;
-    uint256 dueOn;
-    uint256 remindOn;
+import "./User.sol";
 
-    function setName(string calldata _name) public {
-        name = _name;
+contract TaskContract is UserContract {
+    struct Task {
+        uint256 id;
+        uint256 createdBy;
+        uint256 assignedTo;
+        uint8 priority;
+        bool completed;
+        bool pinned;
+        string name;
+        string description;
+        Task[] subTask;
+        string[] labels;
+        uint256 dueOn;
+        uint256 remindOn;
+    }
+    uint256 taskCount = 0;
+    mapping(uint256 => Task) tasks;
+
+    modifier isCreator(uint256 _taskId) {
+        require(msg.sender == getUserAddress(tasks[_taskId].createdBy), "Failed! Task not created by you.");
+        _;
     }
 
-    function setDescription(string calldata _description) public {
-        description = _description;
+    function setName(string calldata _name, uint256 _taskId)
+        public
+        isCreator(_taskId)
+    {
+        tasks[_taskId].name = _name;
     }
 
-    function setLabels(string[] calldata _labels)public{
-        labels=_labels;
+    function setDescription(string calldata _description, uint256 _taskId)
+        public
+        isCreator(_taskId)
+    {
+        tasks[_taskId].description = _description;
+    }
+
+    function setLabels(string[] calldata _labels, uint256 _taskId) public {
+        tasks[_taskId].labels = _labels;
     }
 }
--- blockchain/contracts/User.sol
+++ blockchain/contracts/User.sol
@@ -1,8 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.6;
 
-contract User{
-    address userAddress;
-    string name;
-    uint id;
+contract UserContract{
+
+    uint userCount=0;
+
+    struct User {
+        address userAddress;
+        string name;
+        uint id;
+    }
+    mapping(uint=> User) users;
+
+    modifier userExist{
+        for(uint i = 1; i<=userCount; i++)
+        {
+            if(users[i].userAddress==msg.sender){
+                _;
+            }
+        }
+    }
+
+    function newUser(string calldata _name) public returns (uint){
+        userCount++;
+        users[userCount]=User(msg.sender, _name, userCount);
+        return userCount;
+    }
+
+    function getUserAddress(uint _userId) internal view returns (address){
+        return users[_userId].userAddress;
+    }
+    
 }
