573c611e898a71bf3ca11896592be339e6300b6d
nikitkhakholia <khakholia.nk@gmail.com>
2022-11-18 23:41:16 +0530

    fix

--- blockchain/contracts/User.sol
+++ blockchain/contracts/User.sol
@@ -1,34 +1,41 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.17;
 
-contract UserContract{
-
-    uint userCount=0;
+contract UserContract {
+    uint userCount = 0;
 
     struct User {
         address userAddress;
         string name;
         uint id;
     }
-    mapping(uint=> User) users;
+    mapping(uint => User) users;
 
-    modifier userExist{
-        for(uint i = 1; i<=userCount; i++)
-        {
-            if(users[i].userAddress==msg.sender){
+    modifier userExist() {
+        for (uint i = 1; i <= userCount; i++) {
+            if (users[i].userAddress == msg.sender) {
                 _;
             }
         }
     }
 
-    function newUser(string calldata _name) public returns (uint){
+    function newUser(string calldata _name) public returns (uint) {
         userCount++;
-        users[userCount]=User(msg.sender, _name, userCount);
+        users[userCount] = User(msg.sender, _name, userCount);
         return userCount;
     }
 
-    function getUserAddress(uint _userId) internal view returns (address){
-        return users[_userId].userAddress;
+    // function getUserAddress(uint _userId) internal view returns (address) {
+    //     return users[_userId].userAddress;
+    // }
+
+    function chekUserExist(address _userAddress) internal view returns(bool _exist){
+        _exist=false;
+        for (uint i = 1; i <= userCount; i++) {
+            if (users[i].userAddress == _userAddress) {
+                _exist=true;
+                break;
+            }
+        }
     }
-    
 }
